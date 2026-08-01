f25bcb491f67e701cdc79862f068133bda3338b5
Iqtidar <Miriqtidar026@gmail.com>
2026-08-01 10:56:35 +0530

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import FloatingWhatsApp from "@/components/FloatingWhatsApp";
 
@@ -135,6 +136,22 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2WMD089NWV"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2WMD089NWV');
+          `}
+        </Script>
+
         {children}
 
         <FloatingWhatsApp />
